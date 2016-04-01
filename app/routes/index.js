e903db3c74a81fa538ffdce7f67c4dff9ab271d0
chalmie <blairchalmers@gmail.com>
2016-04-01 09:06:36 -0700

    add new post component

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      posts: this.store.findAll('post'),
+      answers: this.store.findAll('answer')
+    });
+  },
+  actions: {
+    savePost(params) {
+        var newPost = this.store.createRecord('post', params);
+        newPost.save();
+        this.transitionTo('index');
+    }
+  }
 });
